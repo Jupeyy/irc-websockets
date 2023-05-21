45d6c56e00d046337981e08894ae276f6d9236c6
ChillerDragon <ChillerDragon@gmail.com>
2023-05-21 17:02:42 +0200

    Add optional and toggable password to enter
    Also add usernames

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import irc = require('irc')
 import { Socket } from 'socket.io'
-import { ClientToServerEvents, ServerToClientEvents, IrcMessage } from './socket.io'
-import express from 'express'
+import { ClientToServerEvents, ServerToClientEvents, IrcMessage, AuthRequest, AuthResponse } from './socket.io'
+import express, { Request, Response } from 'express'
 import { createServer } from "http";
 import { Server } from "socket.io";
+import bodyParser from 'body-parser';
 const cors = require('cors')
 
 const app = express()
@@ -15,6 +16,48 @@ const io = new Server(httpServer, {
 })
 
 require('dotenv').config()
+
+const generateToken = (len: number): string => {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let token = ''
+  for(let i = 0; i < len; i++) {
+      token += chars[Math.floor(Math.random() * chars.length)]
+  }
+  return token
+}
+
+const isTrue = (envbool: string) => {
+  return new RegExp('^(1|true|yes|on)$', 'i').test(envbool)
+}
+
+interface User {
+  username: string,
+  sessionToken: string,
+  socket: Socket,
+  loggedIn: boolean
+}
+interface UserList {
+  [index: string]: User
+}
+
+/*
+  users
+
+  list of currently connected users
+*/
+const users: UserList = {}
+
+const getUserByName = (username: string): User | null => {
+  if (username === '') {
+    return null
+  }
+  return Object.values(users).find((user) => user.loggedIn && user.username === username) || null
+}
+
+const getUserBySocket = (socket: Socket): User | null => {
+  return users[socket.id]
+  // return users.find((user) => user.loggedIn && user.socket.id === socket.id) || null
+}
 
 // log of last 10 messages
 const messageRobin: IrcMessage[] = []
@@ -34,6 +77,30 @@ if (!process.env.IRC_SERVER) {
 	console.log('Error: IRC_SERVER is not set! check your .env file')
 	process.exit(1)
 }
+if (!process.env.ACCOUNTS_PASSWORD) {
+	console.log('Error: ACCOUNTS_PASSWORD is not set! check your .env file')
+	process.exit(1)
+}
+if (!process.env.ADMIN_TOKEN) {
+	console.log('Error: ADMIN_TOKEN is not set! check your .env file')
+	process.exit(1)
+}
+if (process.env.ADMIN_TOKEN === 'xxx') {
+  console.log('Error: using the default ADMIN_TOKEN is not allowed')
+  process.exit(1)
+}
+
+interface Config {
+  requirePasswords: boolean
+}
+
+const config: Config = {
+  requirePasswords: isTrue(process.env.ACCOUNTS || '0')
+}
+
+const useAccounts = (): boolean => {
+  return config.requirePasswords
+}
 
 const client = new irc.Client(process.env.IRC_SERVER, 'ws-client', {
 	channels: [`#${process.env.IRC_CHANNEL}`],
@@ -45,6 +112,12 @@ client.addListener('error', (message) => {
 
 // http
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', '*')
@@ -57,7 +130,54 @@ app.get('/messages', (req, res) => {
   res.end(JSON.stringify(messageRobin))
 })
 
+const checkAdminAuth = (req: Request, res: Response): boolean => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    res.send(JSON.stringify({
+      error: 'Authentication is required please set the bearer authorization header.'
+    }))
+    return false
+  }
+  const authToken = authHeader.slice('Bearer '.length)
+  console.log(`Auth token: ${authToken}`)
+  if (authToken !== process.env.ADMIN_TOKEN) {
+    res.send(JSON.stringify({
+      error: 'Invalid API key provided'
+     }))
+    return false
+  }
+  return true
+}
+
+// curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer youradmintoken" --data '{"required": true }' http://localhost:6969/admin/password
+app.post('/admin/password', (req: Request, res: Response) => {
+  if (!checkAdminAuth(req, res)) {
+    return
+  }
+  console.log(req.body)
+  if (!req.body || req.body.required === undefined) {
+    res.send({
+      error: "missing key 'required'"
+    })
+    return
+  }
+  const { required } = req.body
+  config.requirePasswords = required ? true : false
+  console.log(`[*] admin set password required to ${required}`)
+  res.send({
+    message: 'OK'
+  })
+})
+
 // ws
+
+const checkAuth = (message: IrcMessage): boolean => {
+  const user = getUserByName(message.from)
+  if (!user) {
+    return false
+  }
+  return user.sessionToken === message.token
+}
 
 client.addListener(`message#${process.env.IRC_CHANNEL}`, (from, message) => {
   console.log(from + ' => #yourchannel: ' + message)
@@ -70,11 +190,75 @@ io.on('connection', (socket: Socket<ClientToServerEvents, ServerToClientEvents>)
     const ipAddr = socket.client.conn.remoteAddress
     const userAgent = socket.handshake.headers['user-agent']
     console.log(`[*] connect ${ipAddr} ${userAgent}`)
-  
+    const user: User = {
+      username: 'connecting',
+      sessionToken: generateToken(32),
+      socket: socket,
+      loggedIn: false
+    }
+    users[socket.id] = user
+
+    socket.on('disconnect', (): void => {
+      const user = getUserBySocket(socket)
+      if (user) {
+        console.log(`[*] '${user.username}' left`)
+      } else {
+        console.log(`[*] leave before login ${userAgent}`)
+      }
+      delete users[socket.id]
+    })
+
+    socket.on('authRequest', (auth: AuthRequest): void => {
+      if(getUserByName(auth.username)) {
+        socket.emit(
+          'authResponse',
+          {
+            username: auth.username,
+            token: '',
+            success: false,
+            message: 'this user is already logged in'
+          }
+        )
+        return
+      }
+      if (useAccounts() && process.env.ACCOUNTS_PASSWORD !== auth.password) {
+        socket.emit(
+          'authResponse',
+          {
+            username: auth.username,
+            token: '',
+            success: false,
+            message: 'wrong password'
+          }
+        )
+        return
+      }
+      const user = getUserBySocket(socket)
+      if (!user) {
+        throw 'User not found'
+      }
+      user.username = auth.username
+      user.loggedIn = true
+      console.log(`[*] '${user.username}' logged in`)
+      socket.emit(
+        'authResponse',
+        {
+          username: user.username,
+          token: user.sessionToken,
+          success: true,
+          message: 'logged in'
+        }
+      )
+    })
     socket.on('message', (message: IrcMessage): void => {
+      if(useAccounts() && checkAuth(message)) {
+        console.log(`[!] WARNING invalid token from ${ipAddr} ${userAgent}`)
+        return
+      }
       console.log(`[*] message from ${ipAddr} ${userAgent}`)
-      console.log(`    ${message.message}`)
-      client.say(`#${process.env.IRC_CHANNEL}`, message.message)
+      const messageStr = `<${message.from}> ${message.message}`
+      console.log(`    ${messageStr}`)
+      client.say(`#${process.env.IRC_CHANNEL}`, messageStr)
       logMessage(message)
       io.emit('message', message)
     })
@@ -82,4 +266,5 @@ io.on('connection', (socket: Socket<ClientToServerEvents, ServerToClientEvents>)
 
 httpServer.listen(6969, (): void => {
     console.log('[*] listening on http://localhost:6969')
+    console.log(`[*] accounts are ${useAccounts() ? 'on' : 'off'}`)
 })
