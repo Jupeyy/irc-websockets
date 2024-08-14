816d19b1a96217271c33f9d5c551a4c79467f0c3
ChillerDragon <ChillerDragon@gmail.com>
2024-08-14 17:55:53 +0800

    Add messages table

---
+++ db/migrations/1723626503_create_messages.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS messages(
+  ID                           INTEGER PRIMARY KEY AUTOINCREMENT,
+  -- either it is a dm then this is non zero
+  friend_id                    INTEGER     NOT NULL,
+
+  -- or it is in a channel then both of these are non zero
+  server_id                    INTEGER     NOT NULL,
+  channel_id                   INTEGER     NOT NULL,
+
+  -- message author
+  user_id                      INTEGER     NOT NULL,
+  content                      TEXT        NOT NULL,
+  created_at                   TEXT        NOT NULL,
+  updated_at                   TEXT        NOT NULL,
+);
